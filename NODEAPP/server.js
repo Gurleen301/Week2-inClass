215725b3b3d452c0bf65be02af5c9710d89818bd
Gurleen301 <78660994+Gurleen301@users.noreply.github.com>
2022-05-19 17:46:40 -0400

    created a node app with server.js file

---
+++ NODEAPP/server.js
@@ -0,0 +1,11 @@
+//import http module
+var http = require('http')
+
+function onRequest(request, response){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Gurleen Kaur - 200453813');
+    response.end();
+}
+
+http.createServer(onRequest).listen(8000);
+
